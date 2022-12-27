df6fe3c58dde1f73cb7a56819d8e13f448f260df
Matteo Piccini <gnappo.1@live.it>
2022-12-27 13:26:57 -0800

    Done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,7 @@
+from statistics import mean 
+from functools import reduce 
+import numpy
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +21,27 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food.get("name") for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food['heat_level'] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    return [print(f"{food.get('name')} ({food.get('cuisine')}) | Heat Level: {'🌶' * food.get('heat_level')}") for food in spicy_foods]
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return next((food for food in spicy_foods if food.get('cuisine') == cuisine), [{}])
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    print_spicy_foods(get_spiciest_foods(spicy_foods))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    # return mean([food['heat_level'] for food in spicy_foods])
+    # return sum([[food['heat_level'] for food in spicy_foods]] for food in spicy_foods])/len(spicy_foods)
+    # return numpy.average([food['heat_level'] for food in spicy_foods])
+    return reduce(lambda acc, food: acc + food['heat_level'], spicy_foods, 0) / len(spicy_foods)
+    # Add the 0 as default value or the entire first object will become acc after the first iteration
